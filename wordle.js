8f1e36b6b1cb4d5d9e220b494ec73a178cf165ae
Brinthaa <brinthaa.m@gmail.com>
2025-11-24 11:03:08 +0000

    update

--- wordle.js
+++ wordle.js
@@ -1,7 +1,9 @@
-async function setUp() {
-    let wordOfTheDay = await newWord();
-    initialisedLetterCounts(wordOfTheDay)
+let wordOfTheDay = ""
+let wordOfTheDayLetterCounts = {}
 
+async function setUp() {
+    wordOfTheDay = await newWord();
+    initialisedLetterCounts(wordOfTheDay)
 }
 
 async function newWord() {
@@ -25,6 +27,7 @@ function initialisedLetterCounts(wordOfTheDay) {
     }
  }
     console.log(wordOfTheDayLetterCounts)
+ return wordOfTheDayLetterCounts; 
 }
 setUp()
 
@@ -39,7 +42,7 @@ let currentRowIndex = 0
 let currentCellIndex = 0;
 const cells = Array.from(document.querySelectorAll(".cell"));
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", async function(event) {
     const rowStart = currentRowIndex * cellsPerRow
     const key = event.key;
     if (/^[a-zA-Z]$/.test(key) && currentCellIndex < cellsPerRow) { 
@@ -52,19 +55,44 @@ document.addEventListener("keydown", function(event) {
         console.log("Userguess:", userGuess);
         console.log("Target Word:", wordOfTheDay);
 
+        const isValid = await validateWord(userGuess)
+        if (!isValid) {
+            const rowStart = currentRowIndex * cellsPerRow
+            for (let i = 0; i < cellsPerRow; i++) {
+                const cell = cells[rowStart + i];
+                cell.classList.add("invalid");
+            }
+        alert("Not a valid word")
+        return;
+        }
+    
         
+    let letterCounts = initialisedLetterCounts(wordOfTheDay);
     //colour cells 
     for (let i = 0; i < userGuess.length; i++) {
         const cellId = rowStart + i
         const cell = document.getElementById(cellId)
+
         if (userGuess [i] === wordOfTheDay [i]) {
             cell.classList.add("correct");
-        } else if (wordOfTheDay.includes (userGuess[i])) {
-            cell.classList.add("nearly")
-        } else {
-            cell.classList.add("incorrect");
+            letterCounts[userGuess[i]]--
         }
     }
+
+    for (let i = 0; i < userGuess.length; i++) {
+        const cellId = rowStart + i
+        const cell = document.getElementById(cellId)
+
+        if (!cell.classList.contains("correct")) {
+            if (letterCounts[userGuess[i]] > 0) {
+                cell.classList.add("nearly")
+                letterCounts[userGuess[i]]--
+            } else {
+                cell.classList.add("incorrect");
+            }
+        }
+    }
+  
 
     if (userGuess === wordOfTheDay) {
             alert("Yay! You win :)")
@@ -84,8 +112,6 @@ document.addEventListener("keydown", function(event) {
             done = true;
         }
 
-     
-
     } else if (event.key === "Backspace" && currentCellIndex > 0 ) {
         currentCellIndex--
         const rowStart = currentRowIndex * cellsPerRow;
@@ -94,3 +120,24 @@ document.addEventListener("keydown", function(event) {
         userGuess = userGuess.slice (0, -1);
     }
 })
+
+async function validateWord(word) {
+    try {
+        const response = await fetch ("https://words.dev-apis.com/validate-word", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({word})
+        });
+
+        const data = await response.json();
+        console.log ("Validation:" , data)
+        return data.validWord;
+    } catch (error) {
+        console.error("Error with word validity:" , error);
+        return false;
+    }
+}
+
+
